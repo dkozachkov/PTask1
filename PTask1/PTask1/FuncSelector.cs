1321bd06364803c70ba3b5d361e7afc49a4203a5
Дмитрий Козачков <kozachkovdmitriy@gmail.com>
2015-07-01 21:49:08 +0300

    Добавлен класс выбора стандартных функций (экспоненты и полинома). У экспоненты минимум гарантированно левая граница, для полинома нужно корректно подобрать степень, коэффициенты и промежуток (отсутствует контроль над вводимыми данными).

---
+++ PTask1/PTask1/FuncSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTask1
+{
+    class FuncSelector
+    {
+        private List<double> FuncParams;
+
+        public FuncSelector(){}
+
+        //Не уверен, сработает ли возврат функции, учитывая что параметры лежат в этом классе.
+        public Func<double, double> FuncReturner(int num, List<double> fpars)
+        {
+            FuncParams = fpars;
+            if (num == 0) return Polynom;
+            else return Exponent;
+        }
+
+        private double Exponent(double x)
+        {
+            return Math.Abs(FuncParams[0]) * Math.Exp(x) + FuncParams[1];
+        }
+
+        private double Polynom(double x)
+        {
+            double res=0;
+            for (int i = 0; i < FuncParams.Count; i++)
+                res += FuncParams[i] * Math.Pow(x, i);
+            return res;
+        }
+    }
+}
