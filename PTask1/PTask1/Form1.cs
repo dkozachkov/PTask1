e706f6bdd01d1e8d00c58c0269a7f142a4cd0dd1
Дмитрий Козачков <kozachkovdmitriy@gmail.com>
2015-07-01 22:28:35 +0300

    Начало добавления интерфейса.

--- PTask1/PTask1/Form1.cs
+++ PTask1/PTask1/Form1.cs
@@ -12,9 +12,19 @@ namespace PTask1
 {
     public partial class Form1 : Form
     {
+        private Label Title;
+        private Label Q1;
+        private Label Q2;
+        private CheckedListBox fselector;
+        private Label QEps;
+        private Label Ans;
+        private TextBox par_getter;
+
         public Form1()
         {
             InitializeComponent();
+            this.Size = new Size(640, 480);
+            this.Text = "Минимизация одномерной функции: метод деления пополам";
         }
     }
 }
